65b01daeaa7ccf2ad24523c40f9481fa0012c88b
unknown <fsolerar@ALC-698BL12.usersad.everis.int>
2015-12-02 16:01:35 +0100

    FIXED tests. Load angular module before than the rest of angular elements.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -33,9 +33,10 @@ module.exports = function(config) {
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
-      "app/scripts/**/*.js",
-      "test/mock/**/*.js",
-      "test/spec/**/*.js"
+      'app/scripts/app.js',
+      'app/scripts/**/*.js',
+      'test/mock/**/*.js',
+      'test/spec/**/*.js'
     ],
 
     // list of files / patterns to exclude
